70b899b3f979b57580a9b6f4eadd831ec08ca588
Pawel Sakaluk <paul.jackals@gmail.com>
2020-12-17 20:18:53 +0100

    ex1 file handler

--- src/ex1_file/file_handler.py
+++ src/ex1_file/file_handler.py
@@ -1,16 +1,36 @@
+import os
+
+
 class FileHandler:
-    def __init__(self, open_function=None):
+    def __init__(self, open_function=None, remove_function=None):
         if open_function is None:
             self.open_function = open
         else:
             self.open_function = open_function
+        if remove_function is None:
+            self.remove_function = os.remove
+        else:
+            self.remove_function = remove_function
 
     def read(self, path):
-        with self.open_function(path) as file:
-            data = file.read()
-        return data
+        try:
+            with self.open_function(path) as file:
+                data = file.read()
+            return data
+        except OSError:
+            raise OSError("Can't access the file")
 
     def write(self, path, text):
-        with self.open_function(path, 'w') as file:
-            file.write(text)
-        return text
+        try:
+            with self.open_function(path, 'w') as file:
+                file.write(text)
+            return text
+        except OSError:
+            raise OSError("Can't write to file")
+
+    def remove(self, path):
+        try:
+            self.remove_function(path)
+            return True
+        except OSError:
+            raise OSError("Can't remove the file")
--- tests/test_ex1_file_handler.py
+++ tests/test_ex1_file_handler.py
@@ -1,5 +1,6 @@
 import unittest
 from unittest.mock import mock_open
+from unittest.mock import Mock
 from src.ex1_file.file_handler import FileHandler
 
 
@@ -15,6 +16,40 @@ class TestEx1FileHandler(unittest.TestCase):
         open_function = mock_open()
         file_handler = FileHandler(open_function=open_function)
         self.assertEqual(file_handler.write('fake/file', text), text)
+
+    def test_remove(self):
+        remove_function = Mock(return_value=True)
+        file_handler = FileHandler(remove_function=remove_function)
+        self.assertTrue(file_handler.remove('fake/file'))
+
+    def test_open_error(self):
+        def no_file(x):
+            if x == 'fake/nofile':
+                raise OSError
+        open_function = Mock(side_effect=no_file)
+        file_handler = FileHandler(open_function=open_function)
+        with self.assertRaisesRegex(OSError, "^Can't access the file$"):
+            file_handler.read('fake/nofile')
+
+    def test_write_error(self):
+        def no_file(x, txt):
+            if x == 'fake/nofile':
+                raise OSError
+        text = "text"
+        open_function = Mock(side_effect=no_file)
+        file_handler = FileHandler(open_function=open_function)
+        with self.assertRaisesRegex(OSError, "^Can't write to file$"):
+            file_handler.write('fake/nofile', text)
+
+    def test_remove_error(self):
+        def no_file(x):
+            if x == 'fake/nofile':
+                raise OSError
+
+        remove_function = Mock(side_effect=no_file)
+        file_handler = FileHandler(remove_function=remove_function)
+        with self.assertRaisesRegex(OSError, "^Can't remove the file$"):
+            file_handler.remove('fake/nofile')
 
 
 if __name__ == '__main__':
